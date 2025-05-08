e429f538dd37d1306757dcefd4b8b5b8de9006a2
LHKadosa <lelekkadosa@gmail.com>
2025-05-09 00:19:40 +0300

    Init commit

---
+++ src/main/java/net/kadosa/testmod/Kadosa_Test_Mod.java
@@ -0,0 +1,16 @@
+package net.kadosa.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Kadosa_Test_Mod implements ModInitializer {
+	public static final String MOD_ID = "kadosatestmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/kadosa/testmod/Kadosa_Test_ModClient.java
@@ -0,0 +1,10 @@
+package net.kadosa.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Kadosa_Test_ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
