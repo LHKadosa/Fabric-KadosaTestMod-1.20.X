11b7364c04278114e08990dcc25c3998edee287b
LHKadosa <lelekkadosa@gmail.com>
2025-05-09 23:53:30 +0300

    Add Sound Block

--- src/main/java/net/kadosa/testmod/item/ModItemGroups.java
+++ src/main/java/net/kadosa/testmod/item/ModItemGroups.java
@@ -28,6 +28,8 @@ public class ModItemGroups {
                         entries.add(ModBlocks.NETHER_RUBY_ORE);
                         entries.add(ModBlocks.END_STONE_RUBY_ORE);
 
+                        entries.add(ModBlocks.SOOUND_BLOCK);
+
                     }).build());
 
 
