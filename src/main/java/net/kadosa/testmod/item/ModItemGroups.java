9ebdc6e7a915368464da5d7ca5dad2030f859b77
LHKadosa <lelekkadosa@gmail.com>
2025-05-09 21:31:16 +0300

    Add Metal Detector

--- src/main/java/net/kadosa/testmod/item/ModItemGroups.java
+++ src/main/java/net/kadosa/testmod/item/ModItemGroups.java
@@ -17,8 +17,12 @@ public class ModItemGroups {
                     .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
                         entries.add(ModItems.RUBY);
                         entries.add(ModItems.RAW_RUBY);
+
+                        entries.add(ModItems.METAL_DETECTOR);
+
                         entries.add(ModBlocks.RUBY_BLOCK);
                         entries.add(ModBlocks.RAW_RUBY_BLOCK);
+
                         entries.add(ModBlocks.RUBY_ORE);
                         entries.add(ModBlocks.DEEPSLATE_RUBY_ORE);
                         entries.add(ModBlocks.NETHER_RUBY_ORE);
