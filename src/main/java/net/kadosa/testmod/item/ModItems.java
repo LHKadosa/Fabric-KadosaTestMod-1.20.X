c8f80c56a1a1be9d7f3fb78e6094b464b504963a
LHKadosa <lelekkadosa@gmail.com>
2025-05-10 18:11:02 +0300

    Add Ruby Staff

--- src/main/java/net/kadosa/testmod/item/ModItems.java
+++ src/main/java/net/kadosa/testmod/item/ModItems.java
@@ -21,6 +21,9 @@ public class ModItems {
     public static final Item TOMATO = registerItem("tomato", new Item(new FabricItemSettings().food(ModFoodComponents.TOMATO)));
     public static final Item COAL_BRIQUETTE = registerItem("coal_briquette", new Item(new FabricItemSettings()));
 
+    public static final Item RUBY_STAFF = registerItem("ruby_staff",
+            new Item(new FabricItemSettings().maxCount(1)));
+
     /* ///Outdated: Ruby has a separate ItemGroup
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
         entries.add(RUBY);
---
+++ src/main/java/net/kadosa/testmod/mixin/ItemRendererMixin.java
@@ -0,0 +1,25 @@
+package net.kadosa.testmod.mixin;
+
+import net.kadosa.testmod.Kadosa_Test_Mod;
+import net.kadosa.testmod.item.ModItems;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(ItemRenderer.class)
+public abstract class ItemRendererMixin {
+    @ModifyVariable(method = "renderItem", at = @At(value = "HEAD"), argsOnly = true)
+    public BakedModel useRubyStaffModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        if (stack.isOf(ModItems.RUBY_STAFF) && renderMode != ModelTransformationMode.GUI) {
+            return ((ItemRendererAccessor) this).mccourse$getModels().getModelManager().getModel(new ModelIdentifier(Kadosa_Test_Mod.MOD_ID, "ruby_staff_3d", "inventory"));
+        }
+        return value;
+    }
+}
