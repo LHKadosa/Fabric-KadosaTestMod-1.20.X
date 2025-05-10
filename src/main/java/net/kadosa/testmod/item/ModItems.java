c9eb0f5826cb95cb8ee54ab12569b7d21f66706e
LHKadosa <lelekkadosa@gmail.com>
2025-05-10 22:11:16 +0300

    Add Full armor effect

--- src/main/java/net/kadosa/testmod/item/ModItems.java
+++ src/main/java/net/kadosa/testmod/item/ModItems.java
@@ -3,6 +3,7 @@ package net.kadosa.testmod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.kadosa.testmod.Kadosa_Test_Mod;
 import net.kadosa.testmod.item.custom.MetalDetectorItem;
+import net.kadosa.testmod.item.custom.ModArmorItem;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -22,24 +23,24 @@ public class ModItems {
             new Item(new FabricItemSettings().maxCount(1)));
 
     public static final Item RUBY_PICKAXE = registerItem("ruby_pickaxe",
-            new PickaxeItem(ModToolMaterial.RUBY, 2, 2f, new FabricItemSettings()));
+            new PickaxeItem(ModToolMaterials.RUBY, 2, 2f, new FabricItemSettings()));
     public static final Item RUBY_AXE = registerItem("ruby_axe",
-            new AxeItem(ModToolMaterial.RUBY, 3, 1f, new FabricItemSettings()));
+            new AxeItem(ModToolMaterials.RUBY, 3, 1f, new FabricItemSettings()));
     public static final Item RUBY_SHOVEL = registerItem("ruby_shovel",
-            new ShovelItem(ModToolMaterial.RUBY, 0, 0f, new FabricItemSettings()));
+            new ShovelItem(ModToolMaterials.RUBY, 0, 0f, new FabricItemSettings()));
     public static final Item RUBY_SWORD = registerItem("ruby_sword",
-            new SwordItem(ModToolMaterial.RUBY, 5, 3f, new FabricItemSettings()));
+            new SwordItem(ModToolMaterials.RUBY, 5, 3f, new FabricItemSettings()));
     public static final Item RUBY_HOE = registerItem("ruby_hoe",
-            new HoeItem(ModToolMaterial.RUBY, 0, 0f, new FabricItemSettings()));
+            new HoeItem(ModToolMaterials.RUBY, 0, 0f, new FabricItemSettings()));
 
     public static final Item RUBY_HELMET = registerItem("ruby_helmet",
-            new ArmorItem(ModArmorMaterial.RUBY, ArmorItem.Type.HELMET, new FabricItemSettings()));
+            new ModArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.HELMET, new FabricItemSettings())); //This has the FullArmorEffect code
     public static final Item RUBY_CHESTPLATE = registerItem("ruby_chestplate",
-            new ArmorItem(ModArmorMaterial.RUBY, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
     public static final Item RUBY_LEGGINGS = registerItem("ruby_leggings",
-            new ArmorItem(ModArmorMaterial.RUBY, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
     public static final Item RUBY_BOOTS = registerItem("ruby_boots",
-            new ArmorItem(ModArmorMaterial.RUBY, ArmorItem.Type.BOOTS, new FabricItemSettings()));
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.BOOTS, new FabricItemSettings()));
 
     /* ///Outdated: Ruby has a separate ItemGroup
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
